cc8e3f5da66caea07c61092c16c842097242cf2d
Jonathan David <jhernadezcorrea@gmail.com>
2024-02-19 23:11:18 -0500

    adding models and controllers of users

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,54 @@
+const catchError = require('../utils/catchError');
+const User = require('../models/User');
+const jwt = require('jsonwebtoken')
+
+const getAll = catchError(async(req, res) => {
+    const results = await User.findAll();
+    return res.json(results);
+});
+
+const create = catchError(async(req, res) => {
+    const result = await User.create(req.body);
+    return res.status(201).json(result);
+});
+
+const remove = catchError(async(req, res) => {
+    const { id } = req.params;
+    const result = await User.destroy({ where: {id} });
+    if(!result) return res.sendStatus(404);
+    return res.sendStatus(204);
+});
+
+const update = catchError(async(req, res) => {
+    const { id } = req.params;
+
+    delete req.body.password
+    delete req.password.email
+
+    const result = await User.update(
+        req.body,
+        { where: {id}, returning: true }
+    );
+    if(result[0] === 0) return res.sendStatus(404);
+    return res.json(result[1][0]);
+});
+const login = catchError(async (req, res) => {
+    const { email, password } = req.body
+    const user = await User.findOne({ where: { email}})
+    if(!user) return res.status(404).json({ error: 'User not found'})
+    
+    const token = jwt.sign(
+        {user},
+        process.env.TOKEN_SECRET, 
+        { expireIn: "1d"}
+    )
+    return res.json({user, token})
+})
+
+module.exports = {
+    getAll,
+    create,
+    remove,
+    update, 
+    login
+}
